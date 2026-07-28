65103f393c6b764972f58c90997a594cbb8df5ca
coderxuz <coderxuz2009@gmail.com>
2026-07-29 00:48:54 +0500

    db create

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from fastapi import FastAPI
 from backend.auth.controller import router as auth_router
 from backend.firm.controller import router as firm_router
 from backend.category.controller import router as category_router
+from db.connection import Base, engine
 
 
 app = FastAPI(title="B2B Backend API", version="0.1.0",openapi_url="/openapi.json", root_path="/api")
-
 
 app.include_router(auth_router)
 app.include_router(firm_router)
 app.include_router(category_router)
 
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 async def root():
